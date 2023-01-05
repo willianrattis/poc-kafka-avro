18b41486bf18bc7e4e54816d28cb611fb3a8de54
willian-rattis <willian.rattis@hotmail.com>
2023-01-05 12:29:32 -0300

    Primeiro Commit

---
+++ Consumidor/Program.cs
@@ -0,0 +1,17 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    GroupId = "Coleta",
+    BootstrapServers = "localhost:9092"
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+consumer.Subscribe("coleta");
+
+while(true)
+{
+    var result = consumer.Consume();
+    Console.WriteLine($"Mensagem: { result.Message.Key } - { result.Message.Value }");
+}
---
+++ Produtor/Program.cs
@@ -0,0 +1,15 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+var message = new Message<string, string>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = $"Mensagem {DateTime.Now.Second}"
+};
+
+var result = await producer.ProduceAsync("coleta", message);
+
+Console.WriteLine($"{ result.Offset }");
