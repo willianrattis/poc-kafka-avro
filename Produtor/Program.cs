8daf7769cfde11d12017d5185fbca1d9a371e556
willian-rattis <willian.rattis@hotmail.com>
2023-01-05 16:32:09 -0300

    Add schema registry with avro

--- Produtor/Program.cs
+++ Produtor/Program.cs
@@ -1,15 +1,32 @@
-﻿using Confluent.Kafka;
+﻿var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
 
 var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
 
-using var producer = new ProducerBuilder<string, string>(config).Build();
+using var producer = new ProducerBuilder<string, Models.Coleta>(config)
+                        .SetValueSerializer(new AvroSerializer<Models.Coleta>(schemaRegistry))
+                        .Build();
 
-var message = new Message<string, string>
+var message = new Message<string, Models.Coleta>
 {
     Key = Guid.NewGuid().ToString(),
-    Value = $"Mensagem {DateTime.Now.Second}"
+    Value = new Models.Coleta
+    {
+        DeviceId = Guid.NewGuid().ToString(),
+        Bandeira = "Casas Bahia",
+        ProdutoId = "12345",
+        PrecoSite = 199.99,
+        PrecoDesconto = 0.99,
+        Sku = "54321",
+        Origem = "origem",
+        DataEvento = $"{DateTime.Now.ToString()}"
+    }
 };
 
 var result = await producer.ProduceAsync("coleta", message);
 
-Console.WriteLine($"{ result.Offset }");
+Console.WriteLine($"{result.Offset}");
